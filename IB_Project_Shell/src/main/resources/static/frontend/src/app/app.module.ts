0bf5993926ddca5c120a33d9b7e88580ccf90379
Djordje Stevanovic <djordje97.tkd@gmail.com>
2018-06-30 13:23:21 +0200

    Created login and register page

--- IB_Project_Shell/src/main/resources/static/frontend/src/app/app.module.ts
+++ IB_Project_Shell/src/main/resources/static/frontend/src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations"
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatInputModule} from "@angular/material/input"
+import {MatButtonModule} from "@angular/material/button";
+import { RegisterComponent } from './register/register.component'
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCheckboxModule,
+    MatInputModule,
+    MatButtonModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
